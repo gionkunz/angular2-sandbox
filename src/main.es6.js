010e20d3102116422977310fdaf83870ef06b24d
Gion Kunz <gion.kunz@gmail.com>
2015-04-22 11:06:11 +0200

    ES6 Refactor use static accessor for annotations

--- src/main.es6.js
+++ src/main.es6.js
@@ -9,22 +9,24 @@ class App {
   setName(name) {
     this.name = name;
   }
-}
 
-App.annotations = [
-  new Component({
-    selector: 'ng2-sandbox'
-  }),
-  new View({
-    template:`
-      <section role="main">
-        <h1>{{ greeting }}, {{ name }}!</h1>
-        <label for="name">Your Name:
-          <input type="text" [value]="name" (keyup)="setName($event.target.value)"/>
-        </label>
-      </section>
-    `
-  })
-];
+  static get annotations() {
+    return [
+      new Component({
+        selector: 'ng2-sandbox'
+      }),
+      new View({
+        template:`
+          <section role="main">
+            <h1>{{ greeting }}, {{ name }}!</h1>
+            <label for="name">Your Name:
+              <input type="text" [value]="name" (keyup)="setName($event.target.value)"/>
+            </label>
+          </section>
+        `
+      })
+    ];
+  }
+}
 
 bootstrap(App);
