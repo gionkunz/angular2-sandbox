ef504da521e790264d1dc205f1527264f7b0d1d3
Gion Kunz <gion.kunz@gmail.com>
2015-05-01 03:45:53 +0200

    Added simple unless Viewport directive example

--- src/main.es6.js
+++ src/main.es6.js
@@ -1,4 +1,30 @@
-import {Component, View, bootstrap} from 'angular2/angular2';
+import {Component, Viewport, ViewContainer, View, bootstrap} from 'angular2/angular2';
+
+@Viewport({
+  selector: '[unless]',
+  properties: {
+    unless: 'unless'
+  }
+})
+class Unless {
+  viewContainer:ViewContainer;
+  prevCondition:boolean;
+
+  constructor(viewContainer:ViewContainer) {
+    this.viewContainer = viewContainer;
+    this.prevCondition = true;
+  }
+
+  set unless(newCondition) {
+    if (newCondition && !this.prevCondition) {
+      this.prevCondition = true;
+      this.viewContainer.clear();
+    } else if (!newCondition && this.prevCondition) {
+      this.prevCondition = false;
+      this.viewContainer.create();
+    }
+  }
+}
 
 @Component({
   selector: 'ng2-sandbox'
@@ -6,25 +32,16 @@ import {Component, View, bootstrap} from 'angular2/angular2';
 @View({
   template: `
     <section role="main">
-      <h1>{{ greeting }}, {{ name }}!</h1>
-      <label for="name">Your Name:
-        <input type="text" [value]="name" (keyup)="setName($event.target.value)"/>
-      </label>
+      <h1>Viewport Directive Test</h1>
+      <p>I'll only show the following content, if this
+          <input type="checkbox" #show (click)/> is NOT checked</p>
+      <div *unless="show.checked">
+        Hello there!
+      </div>
     </section>
-  `
+  `,
+  directives: [Unless]
 })
-class App {
-  greeting:string;
-  name:string;
-
-  constructor() {
-    this.greeting = 'Hello';
-    this.name = 'World';
-  }
-
-  setName(name) {
-    this.name = name;
-  }
-}
+class App {}
 
 bootstrap(App);
